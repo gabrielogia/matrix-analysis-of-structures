e6e5c111e2000a66fed163277c62fa3edcc6975e
João Gabriel Duarte <joaogabrielduarte96@gmail.com>
2022-02-27 23:53:54 -0300

    creating mass consistent matrix to truss elements

--- src/elements/truss.py
+++ src/elements/truss.py
@@ -2,19 +2,22 @@ import numpy as np
 from numpy.core.fromnumeric import transpose
 
 class TrussElement():
-    def __init__(self, id, Ni, Nf, E, A, I, dirLoad, Qx, Qy) -> None:
+    def __init__(self, id = 0, Ni = 0, Nf = 0, E = 0, A = 0, I = 0, D = 0, dirLoad = 0, Qx = 0, Qy = 0) -> None:
         self.id = int(id)
         self.Ni = int(Ni)
         self.Nf = int(Nf)
         self.E = float(E)
         self.A = float(A)
         self.I = float(I)
+        self.D = float(D)
         self.dirLoad = dirLoad
         self.Qx = float(Qx)
         self.Qy = float(Qy)
         self.L = 0.0
         self.cos = 0.0
         self.sin = 0.0
+        self.ml = [] #matriz de massa da barra no sistema local
+        self.mg = [] #matriz de massa da barra no sistema global
         self.kl = [] #matriz de rigidez da barra no sistema local
         self.kg = [] #matriz de rigidez da barra no sistema global
         self.e = [] #vetor de espaçamento
@@ -26,6 +29,16 @@ class TrussElement():
 
     def setEVector(self, Ni, Nf):
         self.e = Ni.coordsGlobal + Nf.coordsGlobal
+        
+    def setLocalMassMatrix(self):
+        c = self.D*self.A*self.L/6
+        
+        self.ml = np.array([[2*c, 0, c, 0],
+                            [0, 2*c, 0, c],
+                            [c, 0, 2*c, 0],
+                            [0, c, 0, 2*c]])
+        
+        self.mg = np.dot(np.dot(transpose(self.R), self.ml), self.R)
 
     def setLocalStiffnessMatrix(self):
         self.kl = np.array([[(self.E*self.A)/self.L, 0, -(self.E*self.A)/self.L, 0],
